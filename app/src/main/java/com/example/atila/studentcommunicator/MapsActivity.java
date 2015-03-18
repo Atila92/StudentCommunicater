c574c3bc720a69c890f0c039f7f2e309526def38
Atila <atila_sarvari@hotmail.com>
2015-03-18 18:43:39 +0100

    Able to add tags and get notification with distance information

--- app/src/main/java/com/example/atila/studentcommunicator/MapsActivity.java
+++ app/src/main/java/com/example/atila/studentcommunicator/MapsActivity.java
@@ -1,20 +1,40 @@
 package com.example.atila.studentcommunicator;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMarkerDragListener;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
 
-public class MapsActivity extends FragmentActivity {
+public class MapsActivity extends FragmentActivity implements OnMapClickListener, OnMapLongClickListener, OnMarkerDragListener{
 
     GoogleMap mMap; // Might be null if Google Play services APK is not available.
     LatLng myPosition;
+
+    final int RQS_GooglePlayServices = 1;
+    Location otherLocation;
+    boolean markerClicked;
+    PolygonOptions polygonOptions;
+    Polygon polygon;
+    LatLng newPos;
+    public float distance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,6 +91,12 @@ public class MapsActivity extends FragmentActivity {
         mMap = fm.getMap();
         mMap.setMyLocationEnabled(true);
 
+        //listeners for marker and map
+        mMap.setOnMapClickListener(this);
+        mMap.setOnMapLongClickListener(this);
+        mMap.setOnMarkerDragListener(this);
+
+        markerClicked = false;
         //mMap.getUiSettings().setMyLocationButtonEnabled(true);
         LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         Criteria criteria = new Criteria();
@@ -87,11 +113,76 @@ public class MapsActivity extends FragmentActivity {
             //Creating a LatLng object for the current location
             LatLng latLng = new LatLng(latitude, longitude);
 
-            myPosition = new LatLng(latitude, longitude);
+            myPosition = latLng;
 
-
-            mMap.addMarker(new MarkerOptions().position(myPosition).title("Mig"));
+            mMap.addMarker(new MarkerOptions().position(myPosition).title("Atila"));
             System.out.print("DEN ER HER!");
+
         }
+
+    }
+    @Override
+    public void onMapClick(LatLng point) {
+        //tvLocInfo.setText(point.toString());
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(point));
+
+        markerClicked = false;
+
+    }
+
+    @Override
+    public void onMapLongClick(LatLng point) {
+       // tvLocInfo.setText("New marker added@" + point.toString());
+
+        mMap.addMarker(new MarkerOptions()
+                .position(point)
+                .draggable(true).title("Someone"));
+
+        markerClicked = false;
+
+    }
+
+    @Override
+    public void onMarkerDrag(Marker marker) {
+       // tvLocInfo.setText("Marker " + marker.getId() + " Drag@" + marker.getPosition());
+    }
+
+    @Override
+    public void onMarkerDragEnd(Marker marker) {
+        double newLatitude = marker.getPosition().latitude;
+        double newLongitude = marker.getPosition().longitude;
+
+        LatLng newlatLng = new LatLng(newLatitude, newLongitude);
+
+        newPos = newlatLng;
+
+
+        float[] results = new float[1];
+        Location.distanceBetween(myPosition.latitude, myPosition.longitude,
+                newPos.latitude, newPos.longitude, results);
+        System.out.println("----------------------->"+results[0]);
+        distance = results[0];
+        notifyUser();
+
+
+    }
+
+    @Override
+    public void onMarkerDragStart(Marker marker) {
+        //tvLocInfo.setText("Marker " + marker.getId() + " DragStart");
+
+    }
+
+    public void notifyUser(){
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.logo)
+                        .setContentTitle("StudentCommunicator")
+                        .setContentText("Someone is "+distance +"m from you" );
+        int NOTIFICATION_ID = 1;
+        notificationManager.notify(NOTIFICATION_ID, mBuilder.build());
     }
 }
