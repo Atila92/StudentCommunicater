3656b1d098e9840cf1e67e320a2da525cfed7771
Adama <adama_camara@hotmail.dk>
2015-04-22 16:13:25 +0200

    Version som virker!

--- app/src/main/java/com/example/atila/studentcommunicator/listScreenActivity.java
+++ app/src/main/java/com/example/atila/studentcommunicator/listScreenActivity.java
@@ -1,17 +1,34 @@
 package com.example.atila.studentcommunicator;
 
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class listScreenActivity extends ActionBarActivity {
-
-   // private static final String URL = "http://toiletgamez.com/bachelor_db/retrieve.php";
-
+    private static final String URL = "http://toiletgamez.com/bachelor_db/display.php";
     private TextView list;
+
+    ArrayList<HashMap<String, String>> arraylist;
+    private JSONArray users = null;
+
+    //JSON IDS:
+    private static final String TAG_USER = "user";
+    private static final String TAG_NAME = "name";
+    private static final String TAG_LON = "longitude";
+    private static final String TAG_LAT = "latitude";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,30 +37,101 @@ public class listScreenActivity extends ActionBarActivity {
 
         list = (TextView) findViewById(R.id.textView);
 
+        Log.v("iListScreen", "onCreate");
+
+        //   ArrayList<String> user = new ArrayList<String>();
+
+
         //listen skal smides ind i denne her "list" det er alm. textview.
 
     }
-
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_list_screen, menu);
-        return true;
+    protected void onResume() {
+        super.onResume();
+        new listScreen().execute();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        public  class listScreen extends AsyncTask<String, String, String> {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
+            protected String doInBackground(String... args) {
+            Log.d("DoInBackground ","kage ");
+                //Instantiate the arraylist to contain all the JSON data.
+
+                arraylist = new ArrayList<HashMap<String, String>>();
+
+                //J parser
+                JSONParser jsonParser = new JSONParser();
+                JSONObject jsonObject = jsonParser.getJSONFromUrl(URL);
+
+
+                try {
+
+                        users = jsonObject.getJSONArray(TAG_USER);
+
+                    // looping through all posts according to the json object returned
+                    for (int i = 0; i < users.length(); i++) {
+                        JSONObject c = users.getJSONObject(i);
+                        //gets the content of each tag
+                        String name = c.getString("name");
+                        String longitude = c.getString("longitude");
+                        String  latitude = c.getString("latitude");
+
+                        // creating new HashMap
+                        HashMap<String, String> map = new HashMap<String, String>();
+                        map.put(TAG_NAME, name);
+                        map.put(TAG_LON, longitude);
+                        map.put(TAG_LAT, latitude);
+
+                        // adding HashList to ArrayList
+                        arraylist.add(map);
+
+                        //System.out.println(map);
+                        Log.v("listScreen","arraylist:" + arraylist);
+                       // list.setText((CharSequence) arraylist);
+
+
+                    }
+
+
+                 //       JSONArray cookie = jsonObjet.getJSONArray("[user]");
+                  //   String name = (String) jsonObjet.get("name");
+                   //   String longitude = (String) jsonObjet.get("longitude");
+                    //  String  latitude = (String) jsonObjet.get("latitude");
+
+                    //   user.add(name);
+                    //   user.add(longitude);
+                    //   user.add(latitude);
+                    //  list.setText(user);
+
+                //           list.setText(name);
+                 //           list.setText(longitude);
+                   //         list.setText(latitude);
+                    // Log.d("VIRKER IKKE", "msg");
+                //   list.setText((CharSequence) cookie);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(String file_url) {
+
+                //     showProgress(false);
+                // pDialog.dismiss();
+                if (file_url != null) {
+                    Toast.makeText(listScreenActivity.this, file_url, Toast.LENGTH_LONG).show();
+
+                }
+
+
+            }
+
+
         }
-
-        return super.onOptionsItemSelected(item);
     }
-}
+
