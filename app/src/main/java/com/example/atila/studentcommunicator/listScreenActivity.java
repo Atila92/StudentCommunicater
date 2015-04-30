0d3201fde453d562ebfb01185dfdfb4ace64507d
Atila <atila_sarvari@hotmail.com>
2015-04-30 13:24:30 +0200

    alt virker

--- app/src/main/java/com/example/atila/studentcommunicator/listScreenActivity.java
+++ app/src/main/java/com/example/atila/studentcommunicator/listScreenActivity.java
@@ -1,31 +1,27 @@
 package com.example.atila.studentcommunicator;
 
+import android.app.Activity;
+import android.location.Location;
 import android.os.AsyncTask;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 
-public class listScreenActivity extends ActionBarActivity {
+public class listScreenActivity extends Activity {
     private static final String TAG = "com.example.atila.studentcommunicator";
     private static final String URL = "http://toiletgamez.com/bachelor_db/display.php";
-    private ListView listView;
-
-    public ArrayList<String> list = new ArrayList<String>();
+    //public ArrayAdapter<String> arrayAdapter;
+    public ArrayList<User> list = new ArrayList<>();
     private JSONArray users = null;
+    public ArrayList<String> nearbyUsers = new ArrayList<String>();
 
     //JSON IDS:
     private static final String TAG_USER = "user";
@@ -38,71 +34,65 @@ public class listScreenActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_screen);
 
-        listView = (ListView) findViewById(R.id.listView);
+
+
+
         new List().execute();
 
-        //Log.i("listScreen","arraylist:" +list);
-        //new List().execute();
-
-
-
-        //list.setText(arraylist);
-
-        //   ArrayList<String> user = new ArrayList<String>();
-
-
-        //listen skal smides ind i denne her "list" det er alm. textview.
 
     }
     @Override
     protected void onResume() {
         super.onResume();
-
+        Log.i("nearbyusers","arraylist:" +nearbyUsers);
     }
 
         public  class List extends AsyncTask<String, String, String> {
 
-
             protected String doInBackground(String... args) {
             Log.d("DoInBackground ","Den kaldes!");
-                //Instantiate the arraylist to contain all the JSON data.
-               // ArrayList<String> list = new ArrayList<String>();
-                //ArrayList<String> data = new ArrayList<String>();
                 //J parser
                 JSONParser jsonParser = new JSONParser();
                 JSONObject jsonObject = jsonParser.getJSONFromUrl(URL);
 
                 try {
-                        users = jsonObject.getJSONArray(TAG_USER);
-
+                    users = jsonObject.getJSONArray(TAG_USER);
+                    User currentUser = null;
                     // looping through all coordinates according to the json object returned
                     for (int i = 0; i < users.length(); i++) {
                         JSONObject c = users.getJSONObject(i);
                         //gets the content of each tag
+                        String email =c.getString("email");
                         String name = c.getString("name");
                         String longitude = c.getString("longitude");
                         String  latitude = c.getString("latitude");
 
-                        // creating new HashMap
-                        //HashMap<String, String> map = new HashMap<String, String>();
-                        list.add(name);
-                        list.add(latitude);
-                        list.add(longitude);
-                        //map.put(TAG_NAME, name);
-                        //map.put(TAG_LON, longitude);
-                        //map.put(TAG_LAT, latitude);
+                        User user = new User(email, name, Double.parseDouble(longitude), Double.parseDouble(latitude));
 
-                        //listView.setText(list);
+                        if(email.equals(LoginActivity.loginEmail)){
+                            currentUser = user;
+                        }
+                        list.add(user);
 
-                        // adding HashList to ArrayList
-                        //arraylist.add(map);
                     }
-                    Log.i("listScreen","arraylist:" +list);
-                    //List<String> your_array_list = new ArrayList<String>();
-                    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(listScreenActivity.this, android.R.layout.simple_list_item_1, list);
+                    if(currentUser != null){
+                        ArrayList<String> names = new ArrayList<String>();
+                        float[] results = new float[1];
+                        for(User user : list){
+                            if(currentUser.getEmail() == user.getEmail()){
+                                continue;
+                            }
+                            Location.distanceBetween(currentUser.getLatitude(), currentUser.getLongitude(),
+                                    user.getLatitude(), user.getLongitude(), results);
+                            if(results[0] <= 100){
+                                names.add(user.getName());
+                            }
+                        }
+                        nearbyUsers = names;
 
-                    //listView.setAdapter(arrayAdapter);
-                    //listView;
+                        Log.i("listScreen","arraylist:" +names);
+                    }
+
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -112,14 +102,13 @@ public class listScreenActivity extends ActionBarActivity {
             }
 
             @Override
-            protected void onPostExecute(String file_url) {
-
-                //     showProgress(false);
-                // pDialog.dismiss();
-                if (file_url != null) {
-                    Toast.makeText(listScreenActivity.this, file_url, Toast.LENGTH_LONG).show();
-
-                }
+            protected void onPostExecute(String result) {
+                Log.i("listScreen","onpostexecute:" +nearbyUsers);
+                ListView listView;
+                ArrayAdapter arrayAdapter;
+                listView = (ListView) findViewById(R.id.listView);
+                arrayAdapter = new ArrayAdapter<String>(listScreenActivity.this,R.layout.simplerow, nearbyUsers);
+                listView.setAdapter(arrayAdapter);
 
 
             }
